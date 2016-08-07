dd1cb6996a924c4196d1145e1a4adfb3bc5c4db4
matthunz <matthunz2@gmail.com>
2016-08-07 01:14:06 -0400

    Add fuse mountpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate fuse;
+
+use std::env;
+use fuse::Filesystem;
+
+struct RedditFileSystem;
+
+impl Filesystem for RedditFileSystem {
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mountpoint = match env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            println!("Usage: {} <Mountpoint>", env::args().nth(0).unwrap());
+            return;
+        }
+    };
+    fuse::mount(RedditFileSystem, &mountpoint, &[])
 }
