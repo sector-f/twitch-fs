d568d536cb93cddf56c1166c6183def82dc08194
matthunz <matthunz2@gmail.com>
2016-08-07 02:49:44 -0400

    Add method to get subreddit json body

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 extern crate fuse;
 extern crate libc;
 extern crate time;
+extern crate hyper;
+extern crate rustc_serialize;
 
 use std::path::Path;
 use std::env;
+use std::io::prelude::Read;
 use libc::{ENOENT, ENOSYS};
 use time::Timespec;
 use fuse::{FileAttr, FileType, Filesystem, Request, ReplyAttr, ReplyData, ReplyEntry, ReplyDirectory};
+use hyper::client::{Client, Response};
+use rustc_serialize::json::Json;
 
 struct RedditFileSystem;
 
@@ -41,10 +46,26 @@ impl Filesystem for RedditFileSystem {
     fn readdir(&mut self, _req: &Request, ino: u64, fh: u64, offset: u64, mut reply: ReplyDirectory) {
         if ino == 1 {
             if offset == 0 {
+                let subreddit = match env::args().nth(2) {
+                    Some(subreddit) => subreddit,
+                    None => "all".to_string()
+                };
+
+                let url = format!("https://www.reddit.com/r/{}.json", subreddit);
+                println!("{}", url);
+                let mut body = String::new();
+                Client::new()
+                    .get(&url)
+                    .send()
+                    .expect("Couldn't load reddit")
+                    .read_to_string(&mut body);
+
+                println!("{}", body);
+
                 reply.add(1, 0, FileType::Directory, &Path::new("."));
                 reply.add(1, 1, FileType::Directory, &Path::new(".."));
             }
-
+            
             reply.ok();
         } else {
             reply.error(ENOSYS)
