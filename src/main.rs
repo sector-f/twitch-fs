84a6174ceca529b595f91e2c787538b8f13cbd08
matthunz <matthunz2@gmail.com>
2016-08-10 04:32:47 -0400

    Add listing of streams inside a game directory

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ impl TwitchFileSystem {
     fn new() -> TwitchFileSystem {
         let mut attrs = BTreeMap::new();
         let mut inodes = BTreeMap::new();
+        
         let ts = time::now().to_timespec();
         let attr = FileAttr {
             ino: 1,
@@ -41,48 +42,11 @@ impl TwitchFileSystem {
             gid: 0,
             rdev: 0,
             flags: 0
+
         };
         attrs.insert(1, attr);
         inodes.insert("/".to_owned(), 1);
-        let mut body = String::new();
-        Client::new()
-            .get("https://api.twitch.tv/kraken/games/top")
-            .send()
-            .expect("Couldn't load twitch")
-            .read_to_string(&mut body);
-
-        match Json::from_str(&body) {
-            Ok(data) => {
-                let games = data.find("top").unwrap().as_array().unwrap();
-                for (i, game) in games.iter().enumerate() {
-                    let attr = FileAttr {
-                        ino: i as u64 + 2,
-                        size: 0,
-                        blocks: 0,
-                        atime: ts,
-                        mtime: ts,
-                        ctime: ts,
-                        crtime: ts,
-                        kind: FileType::Directory,
-                        perm: 0o644,
-                        nlink: 0,
-                        uid: 0,
-                        gid: 0,
-                        rdev: 0,
-                        flags: 0
-                    };
-                    let name = game
-                        .find_path(&["game", "name"])
-                        .unwrap()
-                        .as_string()
-                        .unwrap();
-
-                    attrs.insert(attr.ino, attr);
-                    inodes.insert(name.to_owned(), attr.ino);
-                }
-            },
-            Err(_) => println!("Twitch returned invalid json")
-        }
+        
         TwitchFileSystem {attrs: attrs, inodes: inodes}
     }
 }
@@ -99,6 +63,7 @@ impl Filesystem for TwitchFileSystem {
     }
 
     fn lookup(&mut self, _req: &Request, parent: u64, name: &Path, reply: ReplyEntry) {
+        
         let inode = match self.inodes.get(name.to_str().unwrap()) {
             Some(inode) => inode,
             None => {
@@ -117,19 +82,131 @@ impl Filesystem for TwitchFileSystem {
 
     fn readdir(&mut self, _req: &Request, ino: u64, fh: u64, offset: u64, mut reply: ReplyDirectory) {
         if offset == 0 {
-            for (game, &inode) in &self.inodes {
-                if inode == 1 { continue; }
-                let offset = inode;
-                reply.add(inode, offset, FileType::RegularFile, &Path::new(game));
-            }
-            reply.add(1, 0, FileType::Directory, &Path::new("."));
-            reply.add(1, 1, FileType::Directory, &Path::new(".."));
-        } else if offset == 1 {
-            reply.add(1, 0, FileType::Directory, &Path::new("u wot m8"));
-        }
+            self.attrs.clear();
+            self.inodes.clear();
 
-        reply.ok();
+            let ts = time::now().to_timespec();
+            let attr = FileAttr {
+                ino: 1,
+                size: 0,
+                blocks: 0,
+                atime: ts,
+                mtime: ts,
+                ctime: ts,
+                crtime: ts,
+                kind: FileType::Directory,
+                perm: 0o755,
+                nlink: 0,
+                uid: 0,
+                gid: 0,
+                rdev: 0,
+                flags: 0
+
+            };
+            self.attrs.insert(1, attr);
+            self.inodes.insert("/".to_owned(), 1);
+
+            if ino == 1 {
+                let mut body = String::new();
+                Client::new()
+                    .get("https://api.twitch.tv/kraken/games/top")
+                    .send()
+                    .expect("Couldn't load twitch")
+                    .read_to_string(&mut body);
+
+                match Json::from_str(&body) {
+                    Ok(data) => {
+                        let games = data.find("top").unwrap().as_array().unwrap();
+                        for (i, game) in games.iter().enumerate() {
+                            let attr = FileAttr {
+                                ino: i as u64 + 2,
+                                size: 0,
+                                blocks: 0,
+                                atime: ts,
+                                mtime: ts,
+                                ctime: ts,
+                                crtime: ts,
+                                kind: FileType::Directory,
+                                perm: 0o644,
+                                nlink: 0,
+                                uid: 0,
+                                gid: 0,
+                                rdev: 0,
+                                flags: 0
+                            };
+                            let name = game
+                                .find_path(&["game", "name"])
+                                .unwrap()
+                                .as_string()
+                                .unwrap();
+
+                            self.attrs.insert(attr.ino, attr);
+                            self.inodes.insert(name.to_owned(), attr.ino);
+                            reply.add(attr.ino, 2, FileType::Directory, &Path::new(name));
+                        }
+                    },
+                    Err(_) => println!("Twitch returned invalid json")
+                }
+            } else {
+                // just for testing
+                let game = "Dota 2";
+                /*for (name, inode) in self.inodes.clone().iter() {
+                  if inode == &ino {
+                  game = name;
+                  return; 
+                  }
+                  }*/
+
+                let mut body = String::new();
+                let url = format!("https://api.twitch.tv/kraken/streams?game={}", game);
+                Client::new()
+                    .get(&url)
+                    .send()
+                    .expect("Couldn't load twitch")
+                    .read_to_string(&mut body);
+
+                match Json::from_str(&body) {
+                    Ok(data) => {
+                        let streams = data.find("streams").unwrap().as_array().unwrap();
+                        for (i, stream) in streams.iter().enumerate() {
+                            let attr = FileAttr {
+                                ino: i as u64 + 30,
+                                size: 0,
+                                blocks: 0,
+                                atime: ts,
+                                mtime: ts,
+                                ctime: ts,
+                                crtime: ts,
+                                kind: FileType::RegularFile,
+                                perm: 0o644,
+                                nlink: 0,
+                                uid: 0,
+                                gid: 0,
+                                rdev: 0,
+                                flags: 0
+                            };
+                            let name = stream
+                                .find_path(&["channel", "name"])
+                                .unwrap()
+                                .as_string()
+                                .unwrap();
+
+                            self.attrs.insert(attr.ino, attr);
+                            self.inodes.insert(name.to_owned(), attr.ino);
+                            reply.add(attr.ino, 2, FileType::RegularFile, &Path::new(name));
+                        }
+                    },
+                    Err(_) => println!("Twitch returned invalid json")
+                }
+            } 
+
+            reply.ok();
+        }
     }
+    fn opendir(&mut self, _req: &Request, ino: u64, _flags: u32, reply: ReplyOpen) {
+        reply.opened(ino, 0);
+    }
+
     fn read(&mut self, _req: &Request, ino: u64, fh: u64, offset: u64, size: u32, reply: ReplyData) {
         reply.data("test".as_bytes());
     }
